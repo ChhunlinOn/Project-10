ce1ab49eefc4b4bae9560d12fa9695efc4c53b57
limsopeak16 <sopheaklim70@gmail.com>
2024-08-01 16:13:40 +0700

    last commit

--- src/pages/bookIssue/BookIssuePage.jsx
+++ src/pages/bookIssue/BookIssuePage.jsx
@@ -1,10 +1,11 @@
-import LisBookIssue from "../../components/BookIssue";
+import BookIssueTable from "../../components/BookIssue";
+
 const BookIssuePage = () => {
   return (
     <div className="min-h-screen  bg-gray-150 ">
       <h1 className="text-3xl font-bold">Member</h1>
       <button className="text-1xl font-bold bg-blue-400 rounded-md mt-8 w-20 h-8 ">Create</button>
-      <LisBookIssue></LisBookIssue>
+      <BookIssueTable></BookIssueTable>
     </div>
 
   );
